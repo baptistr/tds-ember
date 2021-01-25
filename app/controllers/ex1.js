34de6aaa6629c24b4f8955833d163902ca350702
Baptiste Lemonnier <>
2021-01-25 12:51:03 +0100

    compteur de caracteres marche

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -3,41 +3,29 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
-
   MAX = 100;
 
-  @tracked
-  toggle() {
-    'Entrez votre texte';
-  }
+  @tracked content = 'Entrez votre texte';
+  @tracked info = '';
 
-  @tracked
-  info() {
-    '';
-  }
-
-  get style(){
-    if (this.toggle() < 20) return 'warning';
-    if (this.toggle() < 10) return 'danger';
+  get style() {
+    if (size() < 20) return 'warning';
+    else if (size() < 10) return 'danger';
     else return 'info';
   }
 
-  get size(){
-    return this.toggle();
+  get size() {
+    return this.MAX - this.content.length;
   }
 
   @action
-  clear(){
-    return '';
+  clear() {
+    this.content = '';
   }
 
   @action
-  save(){
-
-  }
+  save() {}
 
   @action
-  update(){
-
-  }
+  update() {}
 }
