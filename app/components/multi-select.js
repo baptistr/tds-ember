7dc9738e72ca7b12fed33c1ae972d122ae30c136
Baptiste Lemonnier <>
2021-02-11 11:04:21 +0100

    implementation action/fonction permettant d'interagir avec une liste

--- app/components/multi-select.js
+++ app/components/multi-select.js
@@ -1,13 +1,14 @@
 import Component from '@glimmer/component';
-import {action} from '@ember/object';
-import {tracked} from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class MultiSelectComponent extends Component {
   @tracked elements = []; //a chaque fois que elements sera modifié, @tracked va permettre de recuperer sa valeur dans un tableau
   selected = [];
   id = 'id';
 
-  constructor() { //permet d'utiliser le composant plusieurs fois facilement en appelant le constructeur
+  constructor() {
+    //permet d'utiliser le composant plusieurs fois facilement en appelant le constructeur
     super(...arguments);
     this.elements = this.args.elements;
     this.selected = this.args.selected;
@@ -17,11 +18,14 @@ export default class MultiSelectComponent extends Component {
   @action
   change(event) {
     let select = event.target; //on recupere la valeur html de l'event
-    let selectedIds = [...select.selectedOptions].map((option => option.value); //c'est une liste qui recupere la valeur des ids des options
-    if (this.args.onChange) {//si une valeur à changé ...
+    var selectedIds = [...select.selectedOptions].map((option) => option.value); //c'est une liste qui recupere la valeur des ids des options
+    if (this.args.onChange) {
+      //si une valeur à changé ...
       this.args.onChange(
         this.elements.filter(
-          (elm) => selectedIds.filter((e) => e === elm[this.id]).length > 0)); //recuperer les selectids choisis par l'utilisateur
+          (elm) => selectedIds.filter((e) => e === elm[this.id]).length > 0
+        )
+      ); //recuperer les selectids choisis par l'utilisateur
     }
   }
 }
---
+++ app/controllers/step1.js
@@ -0,0 +1,4 @@
+import Controller from '@ember/controller';
+
+export default class Step1Controller extends Controller {
+}
