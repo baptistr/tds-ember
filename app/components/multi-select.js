eb674d559e3e0adb32621d3e3f836acdee12ebda
Baptiste Lemonnier <>
2021-02-11 09:53:48 +0100

    ajout commentaire dans components & helpers

---
+++ app/components/multi-select.js
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
+
+export default class MultiSelectComponent extends Component {
+  @tracked elements = []; //a chaque fois que elements sera modifié, @tracked va permettre de recuperer sa valeur dans un tableau
+  selected = [];
+  id = 'id';
+
+  constructor() { //permet d'utiliser le composant plusieurs fois facilement en appelant le constructeur
+    super(...arguments);
+    this.elements = this.args.elements;
+    this.selected = this.args.selected;
+    this.id = this.args.identifier || 'id';
+  }
+
+  @action
+  change(event) {
+    let select = event.target; //on recupere la valeur html de l'event
+    let selectedIds = [...select.selectedOptions].map((option => option.value); //c'est une liste qui recupere la valeur des ids des options
+    if (this.args.onChange) {//si une valeur à changé ...
+      this.args.onChange(
+        this.elements.filter(
+          (elm) => selectedIds.filter((e) => e === elm[this.id]).length > 0)); //recuperer les selectids choisis par l'utilisateur
+    }
+  }
+}
--- app/helpers/array-contains.js
+++ app/helpers/array-contains.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function arrayContains(params/*, hash*/) {
-  const [items, value, prop] = params;
-  let id = prop || 'id';
-  return items.filterBy(id, value).length > 0;
+  const [items, value, prop] = params; //on défini des constantes égal au paramètre récupéré
+  let id = prop || 'id'; //si id est nul alors on lui donne la valeur prop
+  return items.filterBy(id, value).length > 0; //on retourne les items qui ont une longueur supérieur à 0
 });
